684ad9152df6c2f5567e2473d93861b55b1aebc5
Jyoti <123319919+21Jyoti21@users.noreply.github.com>
2025-11-28 12:28:35 +0530

    Update CORS configuration to allow all origins

--- backend/index.js
+++ backend/index.js
@@ -6,8 +6,9 @@ const cors = require('cors');
 
 // ✅ Use cors middleware to allow frontend to call backend
 app.use(cors({
-  origin: ['http://localhost:3000','https://go-food-git-main-21jyoti21s-projects.vercel.app','https://go-food-smoky.vercel.app','https://go-food-*.vercel.app'
-  ],credentials: true
+  // origin: ['http://localhost:3000','https://go-food-git-main-21jyoti21s-projects.vercel.app','https://go-food-smoky.vercel.app','https://go-food-*.vercel.app'],
+  origin:true,
+  credentials: true
 }));
 
 // ✅ Middleware to parse JSON
